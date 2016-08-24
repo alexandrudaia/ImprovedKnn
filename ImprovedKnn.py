a7050cc37b120a1f9f92b3e15a9d6d9104081111
Home <machine_learning@ubuntu.ubuntu-domain>
2016-08-24 10:47:14 +0300

    Starter

---
+++ ImprovedKnn.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[11]:
+
+class KineticKnn(object):
+    
+    def  __init__(self,train,kinetic,KLdiv,optim_percent):
+        self.train=train
+        self.test=test
+        self.optimization_percent=optimization_percent
+        self.KLdivergence=Kldivergence
+        self.kinetic=kinetic
+    def compute_kinetic(self):
+        """ to do """
+    def compute_euclidean_dist(self):
+        """to do """
+    def  compute_KLDivergence(self):
+        """ to do"""
+    def predict(self,datan, neighbours):
+        """to do"""
+        
+
+
+# In[ ]:
+
+
+
